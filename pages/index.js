f6222573ab2819d73b18fce598bc14f7699571fe
pierrek6e <pierrecass75@gmail.com>
2022-06-14 13:00:11 +0700

    Head component example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <Head>
+      <title>Next app</title>
+    </Head>
       <Link href="/about">About</Link>
     </div>
   );
